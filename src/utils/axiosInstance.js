d1b78d5715d7519fd2059bf8baae21fdf7151f6d
Deepak Mishra <86044241+LOGAN8MD@users.noreply.github.com>
2026-04-08 20:23:07 +0530

    Switch baseURL for axios instance to localhost

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  // baseURL: 'http://localhost:8080',  // Update with your backend URL
-  baseURL:'https://threemt-server.onrender.com',
+  baseURL: 'http://localhost:8080',  // Update with your backend URL
+  // baseURL:'https://threemt-server.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
